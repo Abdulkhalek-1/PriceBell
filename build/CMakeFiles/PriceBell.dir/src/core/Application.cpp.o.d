fd7916d3d7dea675779c5f73aca2aa7b241f1d40
Abdulkhalek-1 <abdulkhalek.muhammad.work@gmail.com>
2025-04-13 12:47:53 +0200

    Remove autogenerated files and build artifacts from the PriceBell project

--- build/CMakeFiles/PriceBell.dir/src/core/Application.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/PriceBell.dir/src/core/Application.cpp.o: \
- /home/abdulkhalek/Projects/PriceBell/src/core/Application.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/PriceBell.dir/src/core/DataProcessor.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/PriceBell.dir/src/core/DataProcessor.cpp.o: \
- /home/abdulkhalek/Projects/PriceBell/src/core/DataProcessor.cpp \
- /usr/include/stdc-predef.h
